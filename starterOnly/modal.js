d7fb087fa583fec8e2d253b017de2e408e707966
Mehdi Charef <me-charef@hotmail.fr>
2023-11-23 19:14:58 +0100

    added error messages for first name and last name

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -39,3 +39,38 @@ function validate(event) {
     confirmationMessage.classList.toggle('confirmation-message-displayed');
   }, 1500);
 }
+
+//validation longueur champ nom
+function nameValidation() {
+  const name = document.querySelector('#last');
+  const displayNameErrorMessage = document.querySelector('.nameErrorMessage');
+  if (name.value.length < 2) {
+    displayNameErrorMessage.classList.add('errorMessageDisplayed');
+    name.style.border = 'red solid 2px';
+    console.log(name.value.length);
+  } else {
+    displayNameErrorMessage.classList.remove('errorMessageDisplayed');
+    name.style.border = 'none';
+  }
+}
+//validation longueur champ prénom
+
+function firstNameValidation() {
+  const firstName = document.querySelector('#first');
+  const displayFirstNameErrorMessage = document.querySelector(
+    '.firstNameErrorMessage'
+  );
+  if (firstName.value.length < 2) {
+    displayFirstNameErrorMessage.classList.add('errorMessageDisplayed');
+    firstName.style.border = '2px solid red';
+  } else {
+    displayFirstNameErrorMessage.classList.remove('errorMessageDisplayed');
+    firstName.style.border = 'none';
+  }
+}
+
+// vérification date de naissance
+// function birthdateValidation() {
+//   const birthdate = document.querySelector('#birthdate');
+//   console.log(birthdate.value);
+// }
